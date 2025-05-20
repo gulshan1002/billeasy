91a8781e72e55047fdc14503635a561b41ea0026
Gulshan Kumar <gulshan190210@gmail.com>
2025-05-20 12:41:16 +0530

    Testing other endpoints in azure

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const app = require('./app');
 
 const PORT = process.env.PORT;
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
     res.send('API is running...');
 });
+
 
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`);

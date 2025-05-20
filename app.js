0709b2e2fd3fcfbaaf42463caa28912b7e58c352
Gulshan Kumar <gulshan190210@gmail.com>
2025-05-20 11:22:53 +0530

    Database connected

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+module.exports = app;

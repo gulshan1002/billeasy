bccbb66a77d168723d9bdf4091f073e1d8da722b
Gulshan Kumar <gulshan190210@gmail.com>
2025-05-20 11:47:44 +0530

    A default API endpoint has been added

--- app.js
+++ app.js
@@ -8,5 +8,7 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
 module.exports = app;

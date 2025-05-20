5c35b0fcc37a2a99e4ba82dbee6dbda0fbc6bfa2
Gulshan Kumar <gulshan190210@gmail.com>
2025-05-20 12:07:00 +0530

    PORT Changed

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const app = require('./app');
 
 const PORT = process.env.PORT;
 
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`);
 });
